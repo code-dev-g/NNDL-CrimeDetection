ca28136e9eb1db0f99d707958f43bfee35a40293
code-dev-g <code.dev.g@gmail.com>
2023-06-16 10:59:48 +0530

    Review 2

--- web.py
+++ web.py
@@ -6,6 +6,8 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 import re
+import numpy as np
+import copy
 
 # nltk.download('stopwords')
 # nltk.download('punkt')
@@ -38,6 +40,7 @@ def preprocess(text):
     #stopwords removal
     words_stop = []
     for word in words:
+        # word = word.decode("utf-16")
         if word not in stopwords.words('english'):
             words_stop.append(word)
 
@@ -57,13 +60,21 @@ def preprocess(text):
     return text
 
 text = preprocess(text)
+in_text = copy.deepcopy(text)
 
 tfidf = pickle.load(open('./models/tf_idf.pkl', 'rb'))
 text = tfidf.transform([text])
 
 
 # use drop down to select model
-model = st.selectbox('Model', ['SVM', 'KNN', 'Naive Bayes', 'Random Forest', 'Cost Sensitive SVM'])
+model = st.selectbox('Model', ['SVM', 'KNN', 'Naive Bayes', 'Random Forest', 'Cost Sensitive SVM', 'Tensorflow NN'])
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+maxlen=50
+def get_sequences(tokenizer, X_train):
+    sequences = tokenizer.texts_to_sequences(X_train)
+    padded = pad_sequences(sequences, truncating = 'post', padding='post', maxlen=maxlen)
+    return padded
 
 # use button to trigger prediction
 
@@ -72,18 +83,23 @@ if st.button('Predict'):
     # load model
     if model == 'SVM':
         loaded_model = pickle.load(open('./models/svm_classifier.pkl', 'rb'))
+        prediction = loaded_model.predict(text)
+
     elif model == 'KNN':
         loaded_model = pickle.load(open('./models/knn_classifier.pkl', 'rb'))
+        prediction = loaded_model.predict(text)
     elif model == 'Naive Bayes':
         loaded_model = pickle.load(open('./models/nb_classifier.pkl', 'rb'))
+        prediction = loaded_model.predict(text)
     elif model == 'Random Forest':
         loaded_model = pickle.load(open('./models/rf_classifier.pkl', 'rb'))
+        prediction = loaded_model.predict(text)
     elif model == 'Cost Sensitive SVM':
         loaded_model = pickle.load(open('./models/cost_svm_classifier.pkl', 'rb'))
-
-    # predict
-    
-    prediction = loaded_model.predict(text)
+        prediction = loaded_model.predict(text)
+    elif model == "Tensorflow NN":
+        loaded_model = pickle.load(open('./models/rf_classifier.pkl'))
+        prediction = loaded_model.predict(text)
 
     # display
     if prediction == 0:
